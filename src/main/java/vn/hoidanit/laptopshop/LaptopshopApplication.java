9b658924a3b076cefc2f79d93a4b2836c9dddbab
Tien Trung <ngtientrung31@gmail.com>
2024-05-23 22:20:13 +0700

    Add Spring security for page

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+//@SpringBootApplication
+//include >< exclude
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext webByTrung = SpringApplication.run(LaptopshopApplication.class, args);
-		for (String s : webByTrung.getBeanDefinitionNames()) {
-			System.out.println(s);
-		}
+		SpringApplication.run(LaptopshopApplication.class, args);
+
 	}
 
 }
